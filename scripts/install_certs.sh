84c8733bc2e98157829a6a0931c18bffed69b0ee
tdviet <tdviet@gmail.com>
2020-12-26 16:10:50 +0100

    Add certificate bundle and script

---
+++ scripts/install_certs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Link to certificate bundle on Github
+cert_link=https://raw.githubusercontent.com/tdviet/python-requests-bundle-certs/main/certs/ca-certificates.crt
+
+# Get path to Python requests certificate bundle
+cert_path=$(python -m requests.certs)
+
+if [ $? -ne 0 ]; then
+    # Certificate bundle found, first make a backup copy with timestamp
+    timestamp=$(date +%s)
+    cp $cert_path $cert_path.$timestamp.backup
+
+    # then download and overwrite the existing certificate bundle
+    wget $cert_link -O $cert_path
+
+else
+    echo "Certificate bundle not found"
+fi
+
+
+
+
+
